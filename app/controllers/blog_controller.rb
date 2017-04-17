b0f3901c229d0cb631b8ba6a4055f4f43cba48dd
Chandler Harvey <crharvey@email.uscupstate.edu>
2017-04-17 17:38:38 +0000

    Add user microposts

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,8 @@
 class BlogController < ApplicationController
         
     def blog
-        
+         @micropost = current_user.microposts.build if logged_in?
+         @feed_items = current_user.feed.paginate(page: params[:page])
     end
     
 end
